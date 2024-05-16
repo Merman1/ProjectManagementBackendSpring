075145e9e1b039085ab739e119b9bd96958c4f83
Merman1 <jarek003514@outlook.com>
2024-05-16 13:14:36 +0200

    Initial commit

---
+++ src/main/java/com/example/projectmanagementbackendspring/ProjectManagementBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectmanagementbackendspring/ProjectManagementBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
